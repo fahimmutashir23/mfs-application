a48e508d80faf30d639c96fa4e97dbafff5214c5
fahimmutashir23 <taslimit23@gmail.com>
2024-02-26 15:24:44 +0600

    initial setup

---
+++ src/Layouts/Layouts.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+
+const Layouts = () => {
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layouts;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-2xl text-green-700">Home</h1>            
+        </div>
+    );
+};
+
+export default Home;
